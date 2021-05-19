af65af008bae6ae41f0d450510c199c0d190ecc0
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2021-05-20 01:49:22 +0530

    adding styles to videoItems

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../api/youtube';
+import VideoList from './VideoList';
 class App extends React.Component{
     state = { videos : [] };
-    onTermSubmit = async (term) => {
+    onTermSubmit = async term => {
         const response = await youtube.get('/search',{
             params:{
                 q: term
             }
         });
         this.setState( { videos : response.data.items } );
-    };
+     };
     render(){
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
-            I have {this.state.videos.length}
+            <VideoList videos={this.state.videos}/>
             </div>
         );
     }
-}
+};
 
 export default App;
