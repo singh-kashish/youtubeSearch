49bcaec8e94fc195c95b6b7dce6bca0e0dc96324
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2021-06-03 01:50:05 +0530

    Finishing project

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import './VideoItem.css';
+import React from "react";
+import "./VideoItem.css";
 
-const VideoItem = ({video}) => {
-    return( 
-    <div className="video-item item" >
-        <img className="ui image" key={video.id.videoId} src={video.snippet.thumbnails.medium.url}/>
-        <div className="content">
-            <div className="header">{video.snippet.title}
-            </div>
-        </div>
-        </div>
-);
+const VideoItem = ({ video, onVideoSelect }) => {
+  return (
+    <div onClick={() => onVideoSelect(video)} className="video-item item">
+      <img
+        className="ui image"
+        alt={video.snippet.title}
+        key={video.id.videoId}
+        src={video.snippet.thumbnails.medium.url}
+      />
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoItem;
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import VideoItem from './VideoItem';
-const VideoList = ({videos}) => {
-    // props.videos
-    const renderedList = videos.map( video => {
-        return <VideoItem video={video} />;
-    });
-    return <div className="ui relaxed divided list">{renderedList}</div>;
+import React from "react";
+import VideoItem from "./VideoItem";
+const VideoList = ({ videos, onVideoSelect }) => {
+  // props.videos
+  const renderedList = videos.map((video) => {
+    return <VideoItem key={video.id.videoId} onVideoSelect={onVideoSelect} video={video} />;
+  });
+  return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
